e370f57070e43997c8f558e92047d62f3bd4573e
suhail ashraf <suhailashraf282@gmail.com>
2025-10-28 07:02:42 +0530

    solved more problems

--- 100_patterns/patterns_5_to_10.py
+++ 100_patterns/patterns_5_to_10.py
@@ -1,12 +1,47 @@
-
 # 6. Print numbers in reverse (10–1)
-# 7. Print first 10 even numbers
-# 8. Print first 10 odd numbers
-# 9. Print sum of first 10 numbers using loop
-# 10. Print multiplication table of 5
-
-for i in range(11, 1, -1):
+for i in range(10,0 ,-1):
     print(i)
+
+
+
+
+
+# 7. Print first 10 even numbers
+lis  = []
+for i in range(1,20+2):
+    if i % 2 == 0:
+        lis.append(i)
+print(lis)
+
+# 8. Print first 10 odd numbers
+limit  = []
+for _ in range(20):
+    if _ % 2 !=0:
+        limit.append(_)
+print(limit)
+
+
+
+
+
+# 9. Print sum of first 10 numbers using loop
+add = 0
+for i in range(1,11):
+    add = add + i
+    print(add, 'each time', i)
+print(add)
+
+
+
+# 10. Print multiplication table of 5
+# 5x1=5
+for i in range(1,11):
+    print(f'{5} x {i} = {i*5}')
+
+
+
+
+
 
 
 
