efd522ac4a1819d19947e512b722d5309ea5ccc9
suhail ashraf <suhailashraf282@gmail.com>
2025-10-30 12:27:29 +0530

    patterns22 done

--- 100_patterns/file22.py
+++ 100_patterns/file22.py
@@ -1,8 +1,13 @@
 n  = 5 #int(input('enter the number :'))
+for i in range(1, n+1):
+    print(' ' * (n-i) + "*" *i)
 
+# method 2
 
-for i in range(n):
-    for j in range(n-1):
-        print('*', end = '')
+for row in range(1, n+1):
+    for space in range(n - row):
+        print(' ', end = ' ')
+    for stars in range(row):
+        print('*', end = ' ')
     print()
 
