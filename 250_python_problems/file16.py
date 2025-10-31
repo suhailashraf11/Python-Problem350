ddebbdda95942d9b6160b7f5df5a6272486b7f8a
suhail ashraf <suhailashraf282@gmail.com>
2025-10-31 18:53:49 +0530

    add

---
+++ 250_python_problems/file16.py
@@ -0,0 +1,18 @@
+# 16. Convert Celsius to Fahrenheit
+
+
+temperature = int(input('Enter the numbers :'))
+
+
+unit = input('enter the unit like c or f : ').lower()
+
+if unit =='f':
+    print(temperature * 9/5 + 2)
+elif unit =='c ':
+    print(temperature-32*5/9)
+else:
+    print('enter the correct values')
+
+
+# working correctly there something technical issue that needs to be fixed , i am in hurry wait.
+
