41fb5e3bfaeeb2a5e4d87b39a2a99dd946ba7743
suhail ashraf <suhailashraf282@gmail.com>
2025-11-01 12:43:51 +0530

    add to more

---
+++ 100_patterns/file26.py
@@ -0,0 +1,14 @@
+# 26. Square of stars (5×5)
+#
+# *****
+# *****
+# *****
+# *****
+# *****
+
+
+nums = 6
+for row in range(1, nums):
+    for col in range(1,nums):
+        print('*', end ='')
+    print()
