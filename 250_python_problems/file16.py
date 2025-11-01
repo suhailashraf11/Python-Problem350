60e01bc0d726cf14f4765d49ace90a3955053ee0
suhail ashraf <suhailashraf282@gmail.com>
2025-11-01 11:39:19 +0530

    add the commit

--- 250_python_problems/file16.py
+++ 250_python_problems/file16.py
@@ -7,9 +7,13 @@ temperature = int(input('Enter the numbers :'))
 unit = input('enter the unit like c or f : ').lower()
 
 if unit =='f':
-    print(temperature * 9/5 + 2)
-elif unit =='c ':
-    print(temperature-32*5/9)
+    #bodman
+    result_f = temperature * (9/5) + 2
+    print(result_f)
+elif unit =='c':
+    # bodmas
+    result_c = temperature -32*(5/9)
+    print(result_c)
 else:
     print('enter the correct values')
 
