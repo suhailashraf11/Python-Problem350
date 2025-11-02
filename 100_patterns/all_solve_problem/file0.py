259ad8144c7a92b68d861b058d391d42a0015c52
suhail ashraf <suhailashraf282@gmail.com>
2025-11-02 22:07:54 +0530

    new changes

---
+++ 100_patterns/all_solve_problem/file0.py
@@ -0,0 +1,9 @@
+initial  = 1
+total = 1
+lis = []
+for i in range(1, 31):
+    total = total * 2
+    lis.append(total) # append in single line
+    print(total)  # testing each line
+print(total)
+print(lis)
--- 250_python_problems/file20.py
+++ 250_python_problems/file20.py
@@ -2,6 +2,17 @@
 
 
 
-nums = int(input('enter number: '))
+base = int(input('enter number: '))
+expo = int(input('enter the number: '))
+if base == 0:
+    print('Enter number above ',base)
+elif base == 1:
+    print('Number is ',  base)
 
-for i in range()
+initial = []
+for i in range(1,expo+1):
+    
+
+
+
+print(initial)
