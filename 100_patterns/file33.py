f56c4356adab3070dc00279d5d58e3e0e4e89091
suhail ashraf <suhailashraf282@gmail.com>
2025-11-03 15:50:07 +0530

    done few updates

---
+++ 100_patterns/file33.py
@@ -0,0 +1,8 @@
+n = int(input('enter the number: '))
+for i in range(1, n):
+    for j in  range(n-i):
+        print(' ', end = '')
+
+    for s in range(i):
+        print('*', end  = '')
+    print()
