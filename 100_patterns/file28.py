ea8c1c16fd48e02f9316159f29c65b63f27fac59
suhail ashraf <suhailashraf282@gmail.com>
2025-11-04 20:58:08 +0530

    add the commmit

--- 100_patterns/file28.py
+++ 100_patterns/file28.py
@@ -3,3 +3,17 @@
 # * *
 # *   *
 # *****
+
+n = 6
+
+for  row in range(n-1,0,-1):
+    if row ==1:
+        print('*')
+    elif row == 5:
+        print('*'*5)
+    else:
+        space = 2 * row - 3
+        print("*", '_' *space , '*')
+
+
+
