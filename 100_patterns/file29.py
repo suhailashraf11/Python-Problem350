862a3210ff726c380e8e744d6c0400bbdf3fe40a
suhail ashraf <suhailashraf282@gmail.com>
2025-11-05 15:02:58 +0530

    solved

--- 100_patterns/file29.py
+++ 100_patterns/file29.py
@@ -3,3 +3,15 @@
 # *   *
 # * *
 # *
+
+
+nums = 5
+for row in range(nums,0,-1):
+    if row == 5:
+        print('*'*row)
+    elif row == 1:
+        print('*'*row)
+
+    else:
+        space = 2 *  row -3
+        print('*',' '* space,'*')
