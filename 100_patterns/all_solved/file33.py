2d60843a9d4d9054389e5cbe3d42ea0bd3c1e611
suhail ashraf <suhailashraf282@gmail.com>
2025-11-05 15:07:54 +0530

    added

--- 100_patterns/all_solved/file33.py
+++ 100_patterns/all_solved/file33.py
@@ -1,8 +1,11 @@
 n = int(input('enter the number: '))
-for i in range(1, n):
+for i in range(1, n+1):
     for j in  range(n-i):
         print(' ', end = '')
 
     for s in range(i):
         print('*', end  = '')
     print()
+
+
+# Solved
