f5ba5ff8cb4e8c800076d7c3730c68139bb72f1d
suhail ashraf <suhailashraf282@gmail.com>
2025-11-14 22:46:47 +0530

    new added file27

--- 100_patterns/file37.py
+++ 100_patterns/file37.py
@@ -1,4 +1,3 @@
-#
 # 37. Half diamond pattern
 # *
 # **
@@ -8,4 +7,18 @@
 # **
 # *
 
+n = 6
+# for i in range(1,n):
+#     for j in range(i):
+#         print('*', end = '')
+#     print()
+# for i in range(n-2,0,-1):
+#     for j in range(i):
+#         print('*', end= '')
+#     print()
 
+
+for i in range(1, n+1):
+    print('* '* i)
+for j in range(n-1,0 ,-1):
+    print('* '*j)
