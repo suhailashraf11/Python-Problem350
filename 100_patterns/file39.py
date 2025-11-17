fe6a5c8a8326cc67cb544d432c67ae464e0113d1
suhail ashraf <suhailashraf282@gmail.com>
2025-11-17 09:54:28 +0530

    added all new file

---
+++ 100_patterns/file39.py
@@ -0,0 +1,17 @@
+# 39. Alternating 1s and 0s
+# 1
+# 0 1
+# 1 0 1
+# 0 1 0 1
+# 1 0 1 0 1
+
+
+n = 6
+for i in range(1, n):
+    for j in range(i):
+        print('*', end =' ')
+    print()
+
+
+
+
---
+++ 100_patterns/file40.py
@@ -0,0 +1,7 @@
+#
+# 40. Binary triangle pattern
+# 1
+# 0 1
+# 1 0 1
+# 0 1 0 1
+# 1 0 1 0 1
