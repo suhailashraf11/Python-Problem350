c4f09a53571578d87bf1d801cc82b374b0c0f23d
suhail ashraf <suhailashraf282@gmail.com>
2025-11-29 22:18:59 +0530

    add new file

--- 100_patterns/file39.py
+++ 100_patterns/file39.py
@@ -5,14 +5,14 @@
 # 0 1 0 1
 # 1 0 1 0 1
 
-
 n = 6
-for i in range(1, n):
+for i in range(1,n):
     for j in range(i):
-        print('*', end =' ')
+        if j % 2 == 0 and i % 2 == 0  :
+            print(0, end ='')
+        else:
+            print(1, end= '')
+
     print()
 
 
-
-
-print("hello world")
