b3f1989cdbc36a410a883693b0c6de27f46b2065
suhail ashraf <suhailashraf282@gmail.com>
2025-12-01 11:01:08 +0530

    added new file to the git

---
+++ 250_python_problems/file24.py
@@ -0,0 +1,16 @@
+# 24. Find LCM of two numbers
+
+
+num1 = int(input('enter the number: '))
+num2 = int(input('enter the number: '))
+if num1 > num2:
+    inital = num1
+else:
+    inital = num2
+
+start = inital
+print(start)
+while True:
+    if num1 % 2 == 0 and num2 % == 0:
+        print()
+
